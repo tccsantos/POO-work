4b1a18a75a0126fdd057fcec3fc38bfff59a370e
tccsantos <109820840+tccsantos@users.noreply.github.com>
2023-09-11 15:41:02 -0300

    bike
    esqueci de dar update na bike

--- bike.ts
+++ bike.ts
@@ -8,6 +8,8 @@ export class Bike {
         public description: string,
         public ratings: number,
         public imageUrls: string[],
-        public id?: string
+        public disp: boolean,
+        public id?: string,
+        public rent?: Date
     ) {}
 }
